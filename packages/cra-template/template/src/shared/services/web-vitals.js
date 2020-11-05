0c8ffb97cc44acc72fd4d02afe7f4ca7700f02e1
Julien Papini <julien.papini@gmail.com>
2020-11-05 17:03:26 +0100

    cra-template: Adding husky and lint-staged dependencies

--- packages/cra-template/template/src/shared/services/web-vitals.js
+++ packages/cra-template/template/src/shared/services/web-vitals.js
@@ -1,6 +1,6 @@
 const WebVitals = {};
 
-WebVitals.reportWebVitals = onPerfEntry => {
+WebVitals.reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
