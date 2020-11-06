bcc28c853edba951d512462fcc6de8011d45a1a2
Julien Papini <julien.papini@gmail.com>
2020-11-06 13:39:21 +0100

    cra-template: Adding Storybook

--- packages/cra-template/template/src/navigation/components/index.js
+++ packages/cra-template/template/src/navigation/components/index.js
@@ -1,5 +1,5 @@
 export { default as Router } from './routers/router';
 
-export { default as NavigationSuspense } from './suspense/navigation-suspense';
+export { default as NavigationSuspense } from './suspenses/navigation-suspense';
 
 export { default as RootSwitch } from './switches/root-switch';
